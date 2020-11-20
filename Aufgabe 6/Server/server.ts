f57add20f324c3a1091cf1b68b8c86241f31908b
annasophiawagner <anna_wagner9797@web.de>
2020-11-20 11:27:57 +0100

    Aufgabe 6

--- Aufgabe 6/Server/server.ts
+++ Aufgabe 6/Server/server.ts
@@ -1,28 +1,33 @@
 import * as Http from "http";
 import * as Url from "url";
 
-export namespace L06_Hexenkessel_No4 {
-    let server: Http.Server = Http.createServer();
 
-    let port: number | string | undefined = process.env.PORT;
-    if (port == undefined)
-        port = 5001;
+let server: Http.Server = Http.createServer();
 
-    console.log("Server starting on port:" + port);
+let port: number | string | undefined = process.env.PORT;
+if (port == undefined)
+    port = 5001;
 
-    server.listen(port);
-    server.addListener("request", handleRequest);
+console.log("Server starting on port:" + port);
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
+server.listen(port);
+server.addListener("request", handleRequest);
 
-        if (_request.url) {
-            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            let jsonString: string = JSON.stringify(url.query);
-            _response.write(jsonString);
+function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("What's up?");
+
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+
+    if (_request.url) {
+        let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+        for (let key in url.query) {
+            _response.write(key + ":" + url.query[key] + "<br/>");
         }
 
-        _response.end();
+        let jsonString: string = JSON.stringify(url.query);
+        _response.write(jsonString);
     }
+
+    _response.end();
 }
