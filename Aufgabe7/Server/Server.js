85a6664c0bdde79b95f440863057009f83227e61
annasophiawagner <anna_wagner9797@web.de>
2020-12-02 12:15:31 +0100

    server.ts

--- Aufgabe7/Server/Server.js
+++ Aufgabe7/Server/Server.js
@@ -17,16 +17,22 @@ if (port == undefined)
 console.log("Server starting on port:" + port);
 server.listen(port);
 server.addListener("request", handleRequest);
-function handleRequest(_request, _response) {
+async function handleRequest(_request, _response) {
     console.log("What's up?");
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
     if (_request.url) {
         let url = Url.parse(_request.url, true);
-        let recipe = JSON.parse(url.query["recipe"]);
-        recipes.insertOne(recipe);
-        let jsonString = JSON.stringify(recipe);
-        _response.write(jsonString);
+        if (url.query["type"] == "put") {
+            let recipe = JSON.parse(url.query["recipe"]);
+            recipes.insertOne(recipe);
+            let jsonString = JSON.stringify(recipe);
+            _response.write(jsonString);
+        }
+        else if (url.query["type"] == "get") {
+            let AllRecipes = await recipes.find().toArray();
+            _response.write(JSON.stringify(AllRecipes));
+        }
     }
     _response.end();
 }
