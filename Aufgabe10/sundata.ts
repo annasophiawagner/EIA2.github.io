59d85cf7ab1e2f5989a3555a3e22ec9338500da1
annasophiawagner <anna_wagner9797@web.de>
2021-01-09 14:50:30 +0100

    aufgabe 10

--- Aufgabe10/sundata.ts
+++ Aufgabe10/sundata.ts
@@ -10,18 +10,18 @@ namespace SKIPISTE {
     }
 
     public draw(){
-    crc2.beginPath();
-    crc2.arc(this.x, this.y, 40, 0, 2 * Math.PI);
-    crc2.fillStyle = "yellow";
-    crc2.fill();
-    crc2.closePath();
+        crc2.beginPath();
+        crc2.arc(this.x, this.y, 40, 0, 2 * Math.PI);
+        crc2.fillStyle = "yellow";
+        crc2.fill();
+        crc2.closePath();
     }
 
     public update(){
-        this.draw();
-    if (this.x > 800) {
-        this.x = 0
-    } 
-    this.x += 1;
+            this.draw();
+        if (this.x > 800) {
+            this.x = 0
+        } 
+        this.x += 1;
     }
 }}
